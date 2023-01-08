1eb9c2f572fc9069d5ba8b631cbaa1f0b339accb
= <moonkey48@icloud.com>
2023-01-08 22:08:07 +0900

    [fix] fix error to deploy

--- src/components/boxs/boxMainRooms.tsx
+++ src/components/boxs/boxMainRooms.tsx
@@ -1,12 +1,12 @@
 import { RoomInfoT } from '../../types/roomTypes';
 import s from './boxs.module.css';
 
-type BoxMainRooms = {
+type BoxMainRoomsProps = {
     roomId: string;
     room: RoomInfoT;
     handleClick: (roomId:string)=>void;
 }
-const BoxMainRooms = ({roomId, room, handleClick}:BoxMainRooms) => {
+const BoxMainRooms = ({roomId, room, handleClick}:BoxMainRoomsProps) => {
     return <li className={s.roomItem} onClick={()=>handleClick(roomId)}>
         <h3 className={s.roomName}>{room.roomName}</h3>
         <h4 className={s.roomType}>{room.roomGameType}</h4>
